84a5bd4fcad225cc5376edccc6df5d8ec697e953
adityavkulkarni <adityavkulkarni0999@gmail.com>
2021-05-14 11:53:03 +0530

    Code cleaning

---
+++ LPR/paths.py
@@ -0,0 +1,11 @@
+import os
+
+YOLO_WEIGHT_PATH = os.getcwd()+'/model/yolov3.weights'
+YOLO_CFG_PATH = os.getcwd()+'/model/yolov3.cfg'
+
+LAPI_WEIGHT_PATH = os.getcwd()+'/model/lapi.weights'
+LAPI_CFG_PATH = os.getcwd()+'/model/lapi.cfg'
+LAPI_NAMES_PATH = os.getcwd()+'/model/lapi_class.names'
+
+IMAGE_OUTPUT_DIR = os.getcwd()+'/op/img/'
+VIDEO_OUTPUT_DIR = os.getcwd()+'/op/vid/'
